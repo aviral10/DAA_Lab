a517ba9073088861b7951afe5a0699a0debe0065
aviral10 <aviral19rana@gmail.com>
2021-08-18 12:42:35 +0530

    Added few files

---
+++ Week_4/problem_1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    //
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    freopen("inp1.txt", "r", stdin);
+    freopen("out1.txt", "w", stdout);
+    //
+
+    int t;
+    cin >> t;
+    while(t--){
+        
+            
+    }
+
+    return 0;
+}
