ef484d04806589490032101ca7b74fe6f11d2c6e
aviral10 <aviral19rana@gmail.com>
2021-08-19 22:41:54 +0530

    Week_4 Completed

--- Week_4/problem_2.cpp
+++ Week_4/problem_2.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sort an array using QuickSort and calculate the number of swaps made
+// Time Complexity: O(nlogn), Space Complexity: O(n)
+
+int partition(vector<int> &arr, int low, int high, int key, int &swaps){
+    int i = low, j = high;
+    while(i < j){
+        while(arr[i] <= arr[key])i++;
+        while(arr[j] > arr[key])j--;
+        if(i < j){
+            swap(arr[i], arr[j]);
+            swaps++;
+        }
+    }
+    swap(arr[key], arr[j]);
+    if(key != j)
+        swaps++;
+    return j;
+}
+
+void quickSort(vector<int> &arr, int low, int high, int &swaps){
+    if(low >= high) return;
+    int n = arr.size();
+    int p = partition(arr, low, high, low, swaps);
+    quickSort(arr, low, p-1,  swaps);
+    quickSort(arr, p+1, high, swaps);
+}
+
 int main(){
     //
     ios_base::sync_with_stdio(false);
@@ -12,8 +39,22 @@ int main(){
     int t;
     cin >> t;
     while(t--){
-        
-            
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+        int comp = 0, inv = 0;
+
+        int swaps = 0;
+        quickSort(arr, 0, n-1, swaps);
+
+        for(auto &x: arr){
+            cout << x << ' ';
+        }
+        cout << '\n';
+        cout << "Swaps: " << swaps << '\n'; 
     }
 
     return 0;
--- Week_4/program_3.cpp
+++ Week_4/program_3.cpp
@@ -1,7 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Given an unsorted array, find out the k-th smallest number 
+// Time Complexity: O(n), Space Complexity: O(1)
 
+
+int partition(vector<int> &arr, int low, int high, int key){
+    int i = low, j = high;
+    while(i < j){
+        while(arr[i] <= arr[key])i++;
+        while(arr[j] >  arr[key])j--;
+        if(i < j)
+            swap(arr[i], arr[j]);
+    }
+    return j;
+}
+
+int kth_element(vector<int> &arr, int k, int low, int high){
+    if(low > high) return -1;
+    int n = arr.size();
+    int p = partition(arr, low, high, low);
+    if(k == p+1) return arr[p];
+    if(k <  p+1) return kth_element(arr, k, low, p-1);
+    else         return kth_element(arr, k, p+1, high); 
+}
 
 int main(){
     //
@@ -14,7 +36,15 @@ int main(){
     int t;
     cin >> t;
     while(t--){
-        
+        int n,k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+
+        int ans = kth_element(arr, k, 0, n-1);
+        cout << ans << '\n';
     }
 
     return 0;
