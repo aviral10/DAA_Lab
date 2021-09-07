c61a8758e7d2eda1e915855abc3f666a42ad3dd5
aviral10 <aviral19rana@gmail.com>
2021-09-07 22:28:45 +0530

    Week6 _p1

---
+++ Week_6/program_3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    //
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    freopen("inp3.txt", "r", stdin);
+    freopen("out3.txt", "w", stdout);
+    //
+    
+
+    return 0;
+}
